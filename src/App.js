53e2208fd90e30fe1b99d347f0739db12db2e0fb
rezzie omoigui <rezzieomoigui.github.io>
2025-06-18 02:24:04 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
@@ -7,23 +6,22 @@ import Discover from "./components/Discover";
 import ArtistSpotlight from "./components/ArtistSpotlight";
 import Genres from "./components/Genres";
 import MyPlaylist from "./components/MyPlaylist";
-import Layout from "./Layout"; // Assuming Layout is in the same directory
-
+import Layout from "./Layout";
 
 const App = () => {
   return (
-     <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="Discover" element={<Discover />} />
-            <Route path="ArtistSpotlight" element={<ArtistSpotlight />} />
-            <Route path="Genres" element={<Genres />} />
-            <Route path="MyPlaylist" element={<MyPlaylist />} />
-
-          </Route>
-        </Routes>
+    <BrowserRouter> {/* basename removed here */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="Discover" element={<Discover />} />
+          <Route path="ArtistSpotlight" element={<ArtistSpotlight />} />
+          <Route path="Genres" element={<Genres />} />
+          <Route path="MyPlaylist" element={<MyPlaylist />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
